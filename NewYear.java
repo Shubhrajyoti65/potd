3605125c12631973ec8f2e5ec10b86646d953fc7
Shubhrajyoti Mohanty <shubhrajyoti2002.bdk@gmail.com>
2024-12-31 00:06:07 +0530

    25/12/2024

---
+++ NewYear.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class  NewYear {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int prob = sc.nextInt();
+        int time = sc.nextInt();
+        int rest=240-time;
+        int restTime=0 , count=0;
+        for(int i=1 ; i<=prob ; i++){
+            restTime += i*5;
+            if(restTime<=rest){
+                count++;
+            }
+        }
+
+System.out.println(count);
+sc.close();
+    }
+}
